769a0726de98b800a81dbb12e004cfb5c3eb2b9d
Ryan Foo <ryanfoo@nyu.edu>
2015-04-20 17:21:30 -0400

    Flying Sheep/Removed some extra folders

--- sheepHero/Assets/SheepScript.cs
+++ sheepHero/Assets/SheepScript.cs
@@ -9,19 +9,28 @@ public class SheepScript : MonoBehaviour {
 	/// <summary>
 	/// Speed of the sheep
 	/// </summary>
-	public Vector2 speed = new Vector2(15,15);
+	public Vector2 speed = new Vector2(1,1);
+
+	// Direction movement 
+	// Sync with tempo grid
+	public Vector2 direction = new Vector2 (0,1);
 
 	// Store Movement
 	private Vector2 movement;
+	
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		// Retrieve axis information
+		// float inputX = Input.GetAxis ("Horizontal");
+		// float inputY = Input.GetAxis ("Vertical");
 		// Movement per direction
+		movement = new Vector2 (speed.x * direction.x, speed.y * direction.y);
+	}
+
+	void FixedUpdate()
+	{
+		// GetComponent.rigidbody2D.velocity = movement;
+		GetComponent<Rigidbody2D> ().velocity = movement;
 	}
 }
